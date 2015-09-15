c7cd1f8ae44b58d64fa5d3d0c1268a20eb9ce408
David Sciamma <david.sciamma@gmail.com>
2015-09-15 17:00:59 +0200

    Add a conf file to extract parameters from main script.

---
+++ conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    twilio: {
+        account_sid: "YOUR_ACCOUNT_SID",
+        auth_token: "YOUR_AUTH_TOKEN"
+    },
+    sms: {
+        to: "PHONE_NUMBER_TO_NOTIFY",
+        from: "TWILIO_PHONE_NUMBER",
+    }
+};
